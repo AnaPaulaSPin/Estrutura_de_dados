934cc4d69584109214147e8328851735cadc434f
AnaPaulaSPin <166036823+AnaPaulaSPin@users.noreply.github.com>
2025-07-02 15:01:40 -0300

    Remover de uma BST
    remover um no de uma arvore binaria e colocar ele numa fila.

---
+++ Unidade 2/Exercicios para Prova/Lista de Exercicios/Exercicio1.cpp
@@ -0,0 +1,2 @@
+// Escreva uma função, em C, para mostrar o conteúdo da folha mais à esquerda, e da folha mais 
+// à direita de uma árvore binária.
---
+++ Unidade 2/Exercicios para Prova/Lista de Exercicios/Exercicio2.cpp
@@ -0,0 +1,2 @@
+// Escreva uma função recursiva, em C, para imprimir todos os nós ascendentes de um certo nó 
+//de uma árvore binária de pesquisa. 
